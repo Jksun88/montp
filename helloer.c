b50cdda90461d871e33c8ee21efeca6526826bb8
Joseph Tankoua <32137285+Jksun88@users.noreply.github.com>
2019-12-16 02:53:41 +0700

    Add files via upload

---
+++ helloer.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+int
+main()
+{
+
+printf("Lancement Rumprun ... \n");
+printf("Version expérimentale\n");
+sleep(5);
+printf("Merci!\n");
+return 0;
+}
---
+++ host_bld/CMakeFiles/helloer.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/helloer.dir/helloer.c.o"
+  "helloer.pdb"
+  "helloer"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/helloer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
